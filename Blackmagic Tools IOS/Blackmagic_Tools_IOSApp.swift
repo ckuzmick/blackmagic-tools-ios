5994bd2a01afa5cfd3649939b39114a912fb9a8b
ckuzmick <conrad.kuzmick@icloud.com>
2023-12-26 17:19:26 -0500

    Initial Commit

---
+++ Blackmagic Tools IOS/Blackmagic_Tools_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blackmagic_Tools_IOSApp.swift
+//  Blackmagic Tools IOS
+//
+//  Created by ck on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Blackmagic_Tools_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
